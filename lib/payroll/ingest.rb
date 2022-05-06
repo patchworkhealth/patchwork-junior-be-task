87f77a165f3238d216bc7679d5d1fbc27856f478
Jonic Linley <jonic@100yen.co.uk>
2022-05-06 15:28:14 +0100

    Lint code so it mostly passes Rubocop checks

--- lib/payroll/ingest.rb
+++ lib/payroll/ingest.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 module Payroll
   class Ingest
     # Task 1 - Ingest data
--- spec/lib/payroll_spec.rb
+++ spec/lib/payroll_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Payroll do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Payroll::VERSION).not_to be nil
   end
 
